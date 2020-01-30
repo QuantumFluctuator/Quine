af1af4264c84e5b84d051a90357700ba314b97c5
Evan Crabtree <ecrab21@gmail.com>
2020-01-30 18:30:42 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  Quine
-//
-//  Created by Evan Crabtree on 26/02/2019.
-//  Copyright © 2019 QuantumFluctuator. All rights reserved.
-//
-
 #include <stdio.h>
 char*s="#include <stdio.h>%cchar*s=%c%s%c;%cint main(void){printf(s,10,34,s,34,10,10);}%c";
 int main(void){printf(s,10,34,s,34,10,10);}
